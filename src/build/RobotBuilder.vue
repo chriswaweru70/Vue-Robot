1c554fa22a52b28bdd581e119856d62a767e2aeb
Chris Waweru <chriswaweru70@gmail.com>
2020-04-04 20:15:39 +0300

    Conditionally applying styles and passing styles from root components to child components

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,35 +1,57 @@
 <template>
-	<div>
+	<div class="content">
+		<button class="add-to-cart" @click="addToCart()">Add to cart</button>
+		<div class="robot-name">
+			{{ selectedRobot.head.title}}
+			<span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
+		</div>
 		<div class="top-row">
-			<div class="top part">
-				<img :src="availableParts.heads[selectedHeadIndex].src" title="head" />
+			<div class="top part" :style="headBorderStyle">
+				<img :src="selectedRobot.head.src" title="head" />
 				<button @click="selectPreviousHead()" class="prev-selector">&#9668;</button>
 				<button @click="selectNextHead()" class="next-selector">&#9658;</button>
 			</div>
 		</div>
 		<div class="middle-row">
 			<div class="left part">
-				<img :src="availableParts.arms[selectedArmLeftIndex].src" title="left arm" />
+				<img :src="selectedRobot.leftArm.src" title="left arm" />
 				<button @click="selectPreviousArm()" class="prev-selector">&#9650;</button>
 				<button @click="selectNextArm()" class="next-selector">&#9660;</button>
 			</div>
 			<div class="center part">
-				<img :src="availableParts.torsos[selectedTorsosIndex].src" title="left arm" />
+				<img :src="selectedRobot.torso.src" title="left arm" />
 				<button @click="selectPreviousTorsos()" class="prev-selector">&#9668;</button>
 				<button @click="selectNextTorsos()" class="next-selector">&#9658;</button>
 			</div>
 			<div class="right part">
-				<img :src="availableParts.arms[selectedArmRightIndex].src" title="left arm" />
+				<img :src="selectedRobot.rightArm.src" title="left arm" />
 				<button @click="selectPreviousArmRight()" class="prev-selector">&#9650;</button>
 				<button @click="selectNextArmRight()" class="next-selector">&#9660;</button>
 			</div>
 		</div>
 		<div class="bottom-row">
 			<div class="bottom part">
-				<img :src="availableParts.bases[selectedBaseIndex].src" title="left arm" />
+				<img :src="selectedRobot.base.src" title="left arm" />
 				<button @click="selectPreviousBases()" class="prev-selector">&#9668;</button>
 				<button @click="selectNextBases()" class="next-selector">&#9658;</button>
 			</div>
+		</div>
+		<div>
+			<h1>Cart</h1>
+			<table>
+				<thead>
+					<tr>
+						<th>Robot</th>
+						<th class="cost">cost</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr v-for="(robot, index) in cart" :key="index">
+						<td>{{ robot.head.title }}</td>
+						<td class="cost">{{ robot.cost }}</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	</div>
 </template>
@@ -52,14 +74,43 @@ export default {
 	data() {
 		return {
 			availableParts,
+			cart: [],
 			selectedHeadIndex: 0,
 			selectedArmLeftIndex: 0,
-			selectedArmRightIndex: 0,
 			selectedTorsosIndex: 0,
+			selectedArmRightIndex: 0,
 			selectedBaseIndex: 0
 		}
 	},
+	computed: {
+		headBorderStyle() {
+			return {
+				border: this.selectedRobot.head.onSale
+					? '3px solid red'
+					: '3px solid #aaa'
+			}
+		},
+		selectedRobot() {
+			return {
+				head: availableParts.heads[this.selectedHeadIndex],
+				leftArm: availableParts.arms[this.selectedArmLeftIndex],
+				torso: availableParts.torsos[this.selectedTorsosIndex],
+				rightArm: availableParts.arms[this.selectedArmRightIndex],
+				base: availableParts.bases[this.selectedBaseIndex]
+			}
+		}
+	},
 	methods: {
+		addToCart() {
+			const robot = this.selectedRobot
+			const cost =
+				robot.head.cost +
+				robot.leftArm.cost +
+				robot.torso.cost +
+				robot.rightArm.cost +
+				robot.base.cost
+			this.cart.push(Object.assign({}, robot, { cost }))
+		},
 		selectNextHead() {
 			this.selectedHeadIndex = getNextValidIndex(
 				this.selectedHeadIndex,
@@ -124,7 +175,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .part {
 	position: relative;
 	width: 165px;
@@ -213,5 +264,31 @@ export default {
 }
 .right .next-selector {
 	right: -3px;
+}
+.robot-name {
+	text-align: center;
+	margin-top: -30px;
+}
+.sale {
+	color: red;
+}
+.content {
+	position: relative;
+}
+.add-to-cart {
+	position: absolute;
+	right: 30px;
+	width: 220px;
+	padding: 3px;
+	font-size: 16px;
+}
+td,
+th {
+	text-align: left;
+	padding: 5px;
+	padding-right: 20px;
+}
+.cost {
+	text-align: right;
 }
 </style>
