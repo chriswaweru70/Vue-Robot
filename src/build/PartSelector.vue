e3f80e69f31f61c3b3bfcc3235ddfbec6dbb0107
Chris Waweru <chriswaweru70@gmail.com>
2020-04-05 16:56:11 +0300

    Using  to pass data from the child component to the parent component

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -46,12 +46,14 @@ export default {
 				this.selectedPartIndex,
 				this.parts.length
 			)
+			this.$emit('partSelected', this.selectedPart)
 		},
 		selectPreviousPart() {
 			this.selectedPartIndex = getPreviousValidIndex(
 				this.selectedPartIndex,
 				this.parts.length
 			)
+			this.$emit('partSelected', this.selectedPart)
 		}
 	}
 }
--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -6,15 +6,35 @@
 			<span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
 		</div>
 		<div class="top-row">
-			<PartSelector :parts="availableParts.heads" position="top" />
+			<PartSelector
+				:parts="availableParts.heads"
+				position="top"
+				@partSelected="part => selectedRobot.head=part"
+			/>
 		</div>
 		<div class="middle-row">
-			<PartSelector :parts="availableParts.arms" position="left" />
-			<PartSelector :parts="availableParts.torsos" position="center" />
-			<PartSelector :parts="availableParts.arms" position="right" />
+			<PartSelector
+				:parts="availableParts.arms"
+				position="left"
+				@partSelected="part => selectedRobot.leftArm=part"
+			/>
+			<PartSelector
+				:parts="availableParts.torsos"
+				position="center"
+				@partSelected="part => selectedRobot.torso=part"
+			/>
+			<PartSelector
+				:parts="availableParts.arms"
+				position="right"
+				@partSelected="part => selectedRobot.rightArm=part"
+			/>
 		</div>
 		<div class="bottom-row">
-			<PartSelector :parts="availableParts.bases" position="bottom" />
+			<PartSelector
+				:parts="availableParts.bases"
+				position="bottom"
+				@partSelected="part => selectedRobot.base=part"
+			/>
 		</div>
 		<div>
 			<h1>Cart</h1>
@@ -64,7 +84,7 @@ export default {
 	methods: {
 		addToCart() {
 			const robot = this.selectedRobot
-			const cost =
+			let cost =
 				robot.head.cost +
 				robot.leftArm.cost +
 				robot.torso.cost +
