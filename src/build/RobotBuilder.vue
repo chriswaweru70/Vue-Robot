e4c9788d8b67e6dc9d36de12811febb4918fc3fc
Chris Waweru <chriswaweru70@gmail.com>
2020-04-05 18:02:54 +0300

    Using slots to pass data from a child component to the parent component

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,19 +1,21 @@
 <template>
 	<div class="content">
 		<div class="preview">
-			<div class="preview-content">
-				<div class="top-row">
-					<img :src="selectedRobot.head.src" />
+			<CollapsibleSection>
+				<div class="preview-content">
+					<div class="top-row">
+						<img :src="selectedRobot.head.src" />
+					</div>
+					<div class="middle-row">
+						<img :src="selectedRobot.leftArm.src" class="rotate-left" />
+						<img :src="selectedRobot.torso.src" />
+						<img :src="selectedRobot.rightArm.src" class="rotate-right" />
+					</div>
+					<div class="bottom-row">
+						<img :src="selectedRobot.base.src" />
+					</div>
 				</div>
-				<div class="middle-row">
-					<img :src="selectedRobot.leftArm.src" class="rotate-left" />
-					<img :src="selectedRobot.torso.src" />
-					<img :src="selectedRobot.rightArm.src" class="rotate-right" />
-				</div>
-				<div class="bottom-row">
-					<img :src="selectedRobot.base.src" />
-				</div>
-			</div>
+			</CollapsibleSection>
 			<button class="add-to-cart" @click="addToCart()">Add to cart</button>
 		</div>
 		<div class="robot-name">
@@ -74,10 +76,11 @@
 <script>
 import availableParts from '../data/parts'
 import PartSelector from './PartSelector'
+import CollapsibleSection from '../shared/CollapsibleSection'
 
 export default {
 	name: 'RobotBuilder',
-	components: { PartSelector },
+	components: { PartSelector, CollapsibleSection },
 	data() {
 		return {
 			availableParts,
